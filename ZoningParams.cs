d98411f92a2b9ce92823aa9d04623a0bc4dd0f9e
Michael O'Connell <michaeljoconnell@ufl.edu>
2020-01-09 15:33:17 -0500

    pls wrk

---
+++ ZoningParams.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum ZoneTypes
+    {
+        General_Residential,
+        Single_Family_House,
+        Public_Services,
+        Commercial,
+        Light_Industrial,
+        Education
+    }
+
+public enum BuildingName
+{
+    Yoga_Studio,
+    Micro_Brewery,
+    Single_Family_House,
+    Two_Story_Duplex,
+    Ten_Story_Apartment,
+    Manufacturing_Facility,
+    Historic_Building,
+    Grocery_Store,
+    Restaurant,
+    Gas_Station,
+    Bar,
+    House_of_Worship,
+    Local_Government_Building,
+    Parking_Lot,
+    Art_Gallery
+}
+public class ZoningParams : MonoBehaviour
+{
+    public BuildingName buildingName;
+    public ZoneTypes[] zoneTypes;
+    
+    [System.Serializable]
+    public class Conditions : MonoBehaviour
+    {
+        public BuildingName[] cantBeAdjacent;
+        public bool heightRestriction;
+        public bool notAllowed;
+    }
+}
